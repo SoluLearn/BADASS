a2a613a31de19ba3fb82cd9b7660ad42341d42e7
“CarlosGomezUribe” <Carlos@solugentech.com>
2024-11-04 16:04:46 -0600

    Replace embeddings.py with updated local version

--- src/EvoProtGrad/embeddings.py
+++ src/EvoProtGrad/embeddings.py
@@ -50,19 +50,11 @@ class OneHotEmbedding(nn.Module):
             embeddings (torch.FloatTensor): Amino acid embeddings of shape [batch_size, max_sequence_len, embedding_dim].
         """
         # convert input_ids to one_hots
-        # one_hots is a torch.FloatTensor of shape [batch_size, max_sequence_len, vocab_size]
-        #one_hots = torch.nn.functional.one_hot(input_ids, num_classes=self.weight.shape[0])
-        #one_hots = one_hots.to(dtype=self.weight.dtype) # to be compatible with half precision ESM2
-        # cache the one_hots
-        #self.one_hots = one_hots.float().requires_grad_()
-        #self.one_hots = one_hots.requires_grad_()
-        #return self.one_hots @ self.weight
         one_hots = torch.nn.functional.one_hot(input_ids, num_classes=self.weight.shape[0])
         one_hots = one_hots.to(dtype=torch.float32)  # Ensure one_hots are in float32 for gradient computation
         # Cache the one_hots
         self.one_hots = one_hots.requires_grad_()
         # Compute the embeddings and convert back to float16 if necessary
-        embeddings = self.one_hots @ self.weight.to(dtype=torch.float32)
-        if self.weight.dtype == torch.float16:
-            embeddings = embeddings.to(dtype=torch.float16)
-        return embeddings
+        with torch.cuda.amp.autocast():
+            embeddings = self.one_hots @ self.weight
+       return embeddings
